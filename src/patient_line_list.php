d9a58243fa3f41f87e4dbe98b3fe42216423cda5
raanki <rabalone94@gmail.com>
2024-04-28 11:00:27 +0200

    wip

--- src/patient_line_list.php
+++ src/patient_line_list.php
@@ -1,26 +1,30 @@
 <?php
-if ($patients)
-{
-    foreach ($patients as $patient)
-    {
+function truncate($string, $length = 15) {
+    if (strlen($string) > $length) {
+        return htmlspecialchars(substr($string, 0, $length)) . "...";
+    } else {
+        return htmlspecialchars($string);
+    }
+}
+
+if ($patients) {
+    foreach ($patients as $patient) {
         echo "<tr>";
-        echo "<td data-id='" . htmlspecialchars($patient['security_number']) . "'>" . (!empty($patient['security_number']) ? htmlspecialchars($patient['security_number']) : '-') . "</td>";
-        echo "<td>" . (!empty($patient['firstname']) ? htmlspecialchars($patient['firstname']) : '-') . "</td>";
-        echo "<td>" . (!empty($patient['second_name']) ? htmlspecialchars($patient['second_name']) : '-') . "</td>";
-        echo "<td>" . (!empty($patient['lastname']) ? htmlspecialchars($patient['lastname']) : '-') . "</td>";
+        echo "<td data-id='" . truncate($patient['security_number']) . "'>" . (!empty($patient['security_number']) ? truncate($patient['security_number']) : '-') . "</td>";
+        echo "<td>" . (!empty($patient['firstname']) ? truncate($patient['firstname']) : '-') . "</td>";
+        echo "<td>" . (!empty($patient['second_name']) ? truncate($patient['second_name']) : '-') . "</td>";
+        echo "<td>" . (!empty($patient['lastname']) ? truncate($patient['lastname']) : '-') . "</td>";
         echo "<td>" . (!empty($patient['sex']) ? htmlspecialchars($patient['sex'] == 'f' ? "Féminin" : "Masculin") : '-') . "</td>";
         echo "<td>" . (!empty($patient['born_date']) ? htmlspecialchars($patient['born_date']) : '-') . "</td>";
-        echo "<td>" . (!empty($patient['email']) ? htmlspecialchars($patient['email']) : '-') . "</td>";
-        echo "<td>" . (!empty($patient['phone']) ? htmlspecialchars($patient['phone']) : '-') . "</td>";
+        echo "<td>" . (!empty($patient['email']) ? truncate($patient['email']) : '-') . "</td>";
+        echo "<td>" . (!empty($patient['phone']) ? truncate($patient['phone']) : '-') . "</td>";
         echo "<td>";
-        echo '<a href="./patient-show?id=' .   htmlspecialchars($patient['security_number']) . '"class="edit"><i class="material-icons" data-toggle="tooltip" title="Modifier">&#xE254;</i></a>';
-        echo '<a href="#deleteEmployeeModal" class="delete btn-line" data-toggle="modal" data-toggle="tooltip" title="Supprimer" data-id="' . htmlspecialchars($patient['security_number']) . '"> <i class="material-icons" >&#xE872;</i></a>';
+        echo '<a href="./patient-show?id=' .   truncate($patient['security_number']) . '" class="edit"><i class="material-icons" data-toggle="tooltip" title="Modifier">&#xE254;</i></a>';
+        echo '<a href="#deleteEmployeeModal" class="delete btn-line" data-toggle="modal" data-toggle="tooltip" title="Supprimer" data-id="' . truncate($patient['security_number']) . '"> <i class="material-icons" >&#xE872;</i></a>';
         echo "</td>";
         echo "</tr>";
     }
-}
-else
-{
+} else {
     echo "<tr><td colspan='9' style='text-align: center;'>Aucun patient trouvé.</td></tr>";
 }
 ?>
