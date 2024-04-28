798647f1a34a3d8b88f3b34bbf01895a2fbd3c82
raanki <rabalone94@gmail.com>
2024-04-28 18:56:44 +0200

    wip

--- src/deletePatient.php
+++ src/deletePatient.php
@@ -6,22 +6,23 @@ $username = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
 $dbname = $_ENV['DB_NAME'];
 
-try
-{
+try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $patientId = $_POST['patientId'];
-    var_dump($patientId);
+
+    $stmtConsult = $conn->prepare("DELETE FROM to_consult WHERE security_number = :patientId AND start_date_slot > NOW()");
+    $stmtConsult->bindParam(':patientId', $patientId);
+    $stmtConsult->execute();
 
     $stmt = $conn->prepare("DELETE FROM patient WHERE security_number = :patientId");
     $stmt->bindParam(':patientId', $patientId);
     $stmt->execute();
 
-    echo "Patient supprimé.";
+    echo "Patient et ses consultations futures supprimées.";
 }
-catch(PDOException $e)
-{
+catch(PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
 
