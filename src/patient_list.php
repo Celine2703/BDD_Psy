fa6d55bd5ca4b11dc312edc360fc409cd84d91b5
raanki <rabalone94@gmail.com>
2024-04-28 17:38:45 +0200

    wip

--- src/patient_list.php
+++ src/patient_list.php
@@ -14,46 +14,74 @@
 <body class="list_patient">
 
 <?php include './header.php'; ?>
-<div class="container" style="">
-	<div class="table-responsive">
-		<div class="table-wrapper">
-			<div class="table-title">
-				<div class="row" style="display: flex; align-items: center; justify-content: space-between;">
-					<div>
-						<h2>Gérer les <b>Patients</b></h2>
-					</div>
 
-				<div>
-					<a href="./patient-create" class="btn btn-success"><i class="material-icons"></i> <span>Ajouter</span></a>
-				</div>
-			</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="filters">
+                <h4 class="mt-5">Filtres</h4>
+                <form id="filters-form">
+                    <div class="form-group">
+                        <label for="age">Âge:</label>
+                        <select class="form-control" id="age" name="age">
+                            <option value="">Tous</option>
+                            <option value="0-12">Moins de 12 ans</option>
+                            <option value="12-18">12 à 18 ans</option>
+                            <option value="18+">Plus de 18 ans</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="sex-filter">Sexe:</label>
+                        <select class="form-control" id="sex-filter" name="sex">
+                            <option value="">Tous</option>
+                            <option value="f">Féminin</option>
+                            <option value="m">Masculin</option>
+                        </select>
+                    </div>
+                    <button type="button" class="btn btn-primary" onclick="applyFilters()">Appliquer</button>
+                </form>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="table-responsive">
+                <div class="table-wrapper">
+                    <div class="table-title">
+                        <div class="row" style="display: flex; align-items: center; justify-content: space-between;">
+                            <div>
+                                <h2>Gérer les <b>Patients</b></h2>
+                            </div>
 
-			</div>
-			<table class="table table-striped table-hover">
+                            <div>
+                                <a href="./patient-create" class="btn btn-success"><i class="material-icons"></i> <span>Ajouter</span></a>
+                            </div>
+                        </div>
 
-                <thead>
-                <tr>
-                    <th data-column="security_number" data-order="asc" class="sortable">Securité social</th>
-                    <th data-column="firstname" data-order="asc" class="sortable">Prénom</th>
-                    <th data-column="second_name" data-order="asc" class="sortable">Second Prénom</th>
-                    <th data-column="lastname" data-order="asc" class="sortable">Nom</th>
-                    <th data-column="sex" data-order="asc" class="sortable">Sexe</th>
-                    <th data-column="born_date" data-order="asc" class="sortable" style="min-width: 130px;">Date de naissance</th>
-                    <th data-column="email" data-order="asc" class="sortable">Email</th>
-                    <th data-column="phone" data-order="asc" class="sortable">Téléphone</th>
-                    <th class="actions">Actions</th>
-                </tr>
-                </thead>
+                    </div>
+                    <table class="table table-striped table-hover">
 
-				<tbody>
-					<!-- pour chaque patient dans la liste des patients on affiche les informations du patient dans une ligne du tableau  -->
-					<?php include './patient_line_list.php'; ?>
-					<!-- end affiche -->
-				</tbody>
+                        <thead>
+                        <tr>
+                            <th data-column="security_number" data-order="asc" class="sortable">Securité social</th>
+                            <th data-column="firstname" data-order="asc" class="sortable">Prénom</th>
+                            <th data-column="second_name" data-order="asc" class="sortable">Second Prénom</th>
+                            <th data-column="lastname" data-order="asc" class="sortable">Nom</th>
+                            <th data-column="sex" data-order="asc" class="sortable">Sexe</th>
+                            <th data-column="born_date" data-order="asc" class="sortable" style="min-width: 130px;">Date de naissance</th>
+                            <th data-column="email" data-order="asc" class="sortable">Email</th>
+                            <th data-column="phone" data-order="asc" class="sortable">Téléphone</th>
+                            <th class="actions">Actions</th>
+                        </tr>
+                        </thead>
 
-			</table>
-		</div>
-</div>
+                        <tbody>
+                        <?php include './patient_line_list.php'; ?>
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 
@@ -74,7 +102,6 @@
                 },
                 success: function(data) {
                     $('tbody').html(data);
-                    // Mettre à jour l'ordre de tri pour refléter le changement
                     $('.sortable[data-column="' + column + '"]').data('order', newOrder);
                 }
             });
